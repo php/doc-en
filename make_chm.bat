768a994035e2a6a6b294eabc3a0e06ae879d28b2
Gabor Hojtsy <goba@php.net>
2001-01-13 15:55:53 +0000

    Make the variable datas ENV vars... compile.bat is now generated by make_chm.php using the PHP_HELP_COMPILE_LANG setting
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@39374 c90b9560-bf6c-de11-be94-00142212c4b1

--- make_chm.bat
+++ make_chm.bat
@@ -1,11 +1,19 @@
 @echo off
-rem First untar the built html tree so that the "html" dir is a child of the
-rem current directory.
 
-rem Now run the toc generator:
-echo "Running the toc and project file generator script:"
-e:\php4\php make_chm.php > manual.hhc
+rem First build the html tree or untar the built html tree,
+rem so that the "html" dir is a child of the current directory.
 
-rem After the toc is generated run the help compiler:
-echo "Compiling the helpfile:"
-e:\htmlhe~1\hhc manual.hhp
+rem Set the needed arguments here
+set PHP_HELP_COMPILER=D:\progra~1\helpwo~1\hhc.exe
+set PHP_HELP_COMPILE_LANG=hu
+set PHP_HELP_COMPILE_DIR=html
+
+echo.
+echo Now running the toc and project file generator script...
+D:\progra~1\php402\php.exe -q make_chm.php
+
+echo Compiling the actual helpfile...
+call compile.bat
+
+echo Done!
+echo.
