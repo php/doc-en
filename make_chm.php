c75ce687099ec7b07d984c5c65f41e1b79b14ffc
Jirka Kosek <jkj@php.net>
2001-02-01 22:48:17 +0000

    - added support for Czech (tested) and for Japanese (not tested) - changed manual.html to $indexfile in a whole script
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@40580 c90b9560-bf6c-de11-be94-00142212c4b1

--- make_chm.php
+++ make_chm.php
@@ -21,12 +21,14 @@
     // Japanese is not on my list, I don't know why...
     $languages = Array (
     
+      "cs"    => "0x405 Czech",
       "de"    => "0x407 German (Germany)",
       "en"    => "0x809 Enlish (United Kingdom)",
       "es"    => "0xc0a Spanish (International Sort)",
       "fr"    => "0x40c French (France)",
       "hu"    => "0x40e Hungarian",
       "il"    => "0x410 Italian (Italy)",
+      "ja"    => "0x411 Japanese",
       "kr"    => "0x412 Korean",
       "nl"    => "0x413 Dutch (Netherlands)",
       "pt_BR" => "0x416 Portuguese (Brazil)"
@@ -56,7 +58,7 @@
     
     // now try to find out how the manual named in the actual language
     // this must be in the manual.html file as the title (DSSSL generated)
-    $content = join("", file("$fancydir/manual.html"));
+    $content = join("", file("$fancydir/$indexfile"));
     if (preg_match("|>(.*)</TITLE|U", $content, $found)) {
       $manual_title = $found[1];
     } else { $manual_title = "PHP Manual"; }
@@ -64,7 +66,7 @@
     fputs ($f, "Title=$manual_title\n");
     
     // define the phpdoc window style (adds more functionality)
-    fputs($f , "\n[WINDOWS]\nphpdoc=\"PHP Manual\",\"manual_hu.hhc\",," .
+    fputs($f , "\n[WINDOWS]\nphpdoc=\"PHP Manual\",\"manual_$language.hhc\",," .
     "\"$fancydir\\$indexfile\",\"$fancydir\\$indexfile\",,,,,0x23520,,0x386e,,,,,,,,0\n");
 
     // write out all the filenames as in $fancydir    
@@ -139,7 +141,7 @@ $tabs  <param name=\"Local\" value=\"$fancydir\\$local\">";
 <UL>
 <?php
   
-  $index_a = file ("$fancydir/manual.html");
+  $index_a = file ("$fancydir/$indexfile");
   $ijoin = join("", $index_a);
   $ijoin = preg_replace("/[\r|\n]/", " ", $ijoin);
   
@@ -150,7 +152,7 @@ $tabs  <param name=\"Local\" value=\"$fancydir\\$local\">";
 
   if ($fancyindex) {
     preg_match('|CLASS=\"title\" ><A NAME=\"manual\" >(.*)</A|U', $ijoin, $match);
-    SiteMapObj($match[1], "manual.html", "  ", 21);
+    SiteMapObj($match[1], "$indexfile", "  ", 21);
   }
 
   preg_match('|<A HREF="preface.html" >(.*)</A >|U', $ijoin, $match);
