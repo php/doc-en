004fc17172cc294d8eb130bec08ba6829d260f44
Hartmut Holzgraefe <hholzgra@php.net>
2001-11-22 22:07:17 +0000

    be more clear about the current manual license
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@63084 c90b9560-bf6c-de11-be94-00142212c4b1

--- genfuncindex.php
+++ genfuncindex.php
@@ -2,28 +2,23 @@
 /*
 # 
 # +----------------------------------------------------------------------+
-# | PHP HTML Embedded Scripting Language Version 3.0                     |
+# | PHP Manual                                                           |
 # +----------------------------------------------------------------------+
-# | Copyright (c) 1997,1998 PHP Development Team (See Credits file)      |
+# | Copyright (c) 2001 PHP Documentation Team                            |
 # +----------------------------------------------------------------------+
 # | This program is free software; you can redistribute it and/or modify |
-# | it under the terms of one of the following licenses:                 |
-# |                                                                      |
-# |  A) the GNU General Public License as published by the Free Software |
-# |     Foundation; either version 2 of the License, or (at your option) |
-# |     any later version.                                               |
-# |                                                                      |
-# |  B) the PHP License as published by the PHP Development Team and     |
-# |     included in the distribution in the file: LICENSE                |
+# | it under the terms of the GNU General Public License as published by |
+# | the Free Software Foundation; either version 2 of the License, or    |
+# | (at your option) any later version.                                  |
 # |                                                                      |
 # | This program is distributed in the hope that it will be useful,      |
 # | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
 # | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
 # | GNU General Public License for more details.                         |
 # |                                                                      |
-# | You should have received a copy of both licenses referred to here.   |
-# | If you did not, or have any questions about PHP licensing, please    |
-# | contact core@php.net.                                                |
+# | You should have received a copy of the license refered to here in a  |
+# | file called LICENSE. If you did not, or have any questions about PHP |
+# | licensing, please contact hartmut@six.de.                            |
 # +----------------------------------------------------------------------+
 # | Authors:    Hartmut Holzgraefe <hartmut@six.de>                      |
 # +----------------------------------------------------------------------+
