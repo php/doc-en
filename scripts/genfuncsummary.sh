4063a503ee2b62c748f00d616ccf9096f13c2e6f
Jan Lehnardt <jan@php.net>
2002-01-09 14:50:45 +0000

    added or renewed license header
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@67420 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/genfuncsummary.sh
+++ scripts/genfuncsummary.sh
@@ -1,5 +1,22 @@
-#!/bin/sh
-# $Id: genfuncsummary.sh,v 1.2 2002-01-07 10:25:09 goba Exp $
+#!/bin/sh# 
+#
+# +----------------------------------------------------------------------+
+# | PHP Version 4                                                        |
+# +----------------------------------------------------------------------+
+# | Copyright (c) 1997-2002 The PHP Group                                |
+# +----------------------------------------------------------------------+
+# | This source file is subject to version 2.02 of the PHP licience,     |
+# | that is bundled with this package in the file LICENCE and is         |
+# | avalible through the world wide web at                               |
+# | http://www.php.net/license/2_02.txt.                                 |
+# | If uou did not receive a copy of the PHP license and are unable to   |
+# | obtain it through the world wide web, please send a note to          |
+# | license@php.net so we can mail you a copy immediately                |
+# +----------------------------------------------------------------------+
+# | Authors:    Gabor Hoitsy <goba@php.net>                              |
+# +----------------------------------------------------------------------+
+#
+# $Id: genfuncsummary.sh,v 1.3 2002-01-09 14:50:45 jan Exp $
 
 if test -f funcsummary.awk; then
   awkscript=funcsummary.awk
