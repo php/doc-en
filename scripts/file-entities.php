b8901859daa3a80fafae830ee156f0dce8b97956
Hartmut Holzgraefe <hholzgra@php.net>
2002-04-07 19:52:12 +0000

    hope this fixes the cygwin problem ...
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@77031 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/file-entities.php
+++ scripts/file-entities.php
@@ -10,11 +10,11 @@ exists
 ob_end_clean();
 ob_implicit_flush();
 
-$base_dir  = abs_path($argv[1]);
+$base_dir  = abs_path($argv[$argc-2]);
 if(php_sapi_name()!="cli") $base_dir=ereg_replace("/scripts$","",$base_dir);
 
 $orig_dir  = $base_dir."/en";
-$trans_dir = $base_dir."/".$argv[2];
+$trans_dir = $base_dir."/".$argv[$argc-1];
 
 process($orig_dir,$trans_dir,$orig_dir);
 
