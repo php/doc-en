d72fb2327a7869eba69a967ecdb25bf34a680fc6
Hartmut Holzgraefe <hholzgra@php.net>
2002-06-11 10:54:23 +0000

    the en/reference/*/functions.xml files are now auto-generated (should have been from the start, but somehow i forgot to  move the generation code from the split to the configure script)
    entities/chapters.ent is now called entities/file-entities.ent
    
    
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@85368 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/file-entities.php
+++ scripts/file-entities.php
@@ -11,13 +11,23 @@ ob_implicit_flush();
 set_time_limit(0);
 
 $base_dir  = str_replace("\\","/",abs_path($argv[$argc-2]));
-# if(php_sapi_name()!="cli") 
-	$base_dir=ereg_replace("/scripts$","",$base_dir);
+$base_dir=ereg_replace("/scripts$","",$base_dir);
 
 $orig_dir  = $base_dir."/en";
 $trans_dir = $base_dir."/".$argv[$argc-1];
 
-process($orig_dir,$trans_dir,$orig_dir);
+$entities = array();
+process($orig_dir, $trans_dir, $orig_dir, $entities);
+
+$fp = fopen("$base_dir/entities/file-entities.ent", "a");
+foreach($entities as $entity) {
+	fputs($fp, "$entity\n");
+}
+fclose($fp);
+
+exit;
+
+
 
 function abs_path($path) {
 	if($path{0} == '/') return $path;
@@ -34,29 +44,53 @@ function abs_path($path) {
 	return join("/",$absdirs);
 }
 
-function process($work_dir, $trans_dir, $orig_dir) {
+function process($work_dir, $trans_dir, $orig_dir, &$entities) {
+
 	$trans_path = str_replace($orig_dir,$trans_dir,$work_dir);
 	$dh = opendir($work_dir);
 	if(!$dh) return false;
+
+	if(ereg("/reference/.*/functions$", $work_dir)) {
+		$function_entities = array();
+		$functions_file = "$work_dir.xml";
+
+		$functions_file_entity = str_replace("$orig_dir/","",$work_dir);
+		$functions_file_entity = str_replace("/",".",$functions_file_entity);
+		$functions_file_entity = str_replace("_","-",$functions_file_entity);
+
+		$entities[] = sprintf("<!ENTITY %-40s SYSTEM '%s'>\n",$functions_file_entity,$functions_file);
+	}
+
 	while(false !== ($file = readdir($dh))) {
-		if($file=="."||$file==".."||$file=="CVS") continue;
-		if(is_dir($work_dir."/".$file)) process($work_dir."/".$file, $trans_dir, $orig_dir);
+
+		if($file{0} == ".") 
+			continue;
+
+		if(is_dir($work_dir."/".$file)) {
+			if($file == "CVS") continue;
+			process($work_dir."/".$file, $trans_dir, $orig_dir, $entities);
+		}
+
 		if(ereg("\\.xml$",$file)) {
       $name = str_replace("$orig_dir/","",$work_dir."/".ereg_replace("\\.xml$","",$file));
 			$name = str_replace("/",".",$name);
 			$name = str_replace("_","-",$name);
 
+			if(isset($function_entities)) {
+				$function_entities[] = "&$name;";
+			}
+			
       // new: special treatment for function reference entities if splitted version available
       if(strstr($work_dir,"/functions")) {
         $splitfile = str_replace(".xml","/reference.xml",$file);
         $splitpath = str_replace("/functions","/reference",$trans_path) . "/" . $splitfile;
         if(file_exists($splitpath)) {
-          echo sprintf("<!ENTITY %-40s SYSTEM '%s'>\n",$name,$splitpath);
+          $entities[] = sprintf("<!ENTITY %-40s SYSTEM '%s'>\n",$name,$splitpath);
           continue;
         } 
         $splitpath = str_replace("/functions","/reference",$trans_path) . "/" . $splitfile;
         if(file_exists($splitpath)) {
-          echo sprintf("<!ENTITY %-40s SYSTEM '%s'>\n",$name,$splitpath);
+          $entities[] = sprintf("<!ENTITY %-40s SYSTEM '%s'>\n",$name,$splitpath);
           continue;
         } 
       }
@@ -66,17 +100,26 @@ function process($work_dir, $trans_dir, $orig_dir) {
 			} else {
 				$path= "$work_dir/$file";
 			}
-			echo sprintf("<!ENTITY %-40s SYSTEM '$path'>\n",$name);
+			$entities[] = sprintf("<!ENTITY %-40s SYSTEM '$path'>\n",$name);
 		}
 	}
 	closedir($dh);
 
+	if(isset($function_entities)) {
+		sort($function_entities);
+		$fp = fopen($functions_file, "w");
+		foreach($function_entities as $entity) {
+			fputs($fp, "$entity\n");
+		}
+		fclose($fp);
+	}
+
   // now find all files available in the translation but not the original
-  if(file_exists($trans_path) && is_dir($trans_path)) {
+  if($orig_dir != $trans_dir && file_exists($trans_path) && is_dir($trans_path)) {
     $dh = @opendir($trans_path);
     if($dh) {
       while(false !== ($file = readdir($dh))) {
-        if($file=="."||$file==".."||$file=="CVS") continue;
+        if($file{0}=="." || $file=="CVS") continue;
         if(is_dir($trans_path."/".$file)) continue;
         if(ereg("\\.xml$",$file)) {
           $name = str_replace("$orig_dir/","",$work_dir."/".ereg_replace("\\.xml$","",$file));
@@ -85,7 +128,7 @@ function process($work_dir, $trans_dir, $orig_dir) {
           
           if(!file_exists("$work_dir/$file")) {
             $path= "$trans_path/$file";
-            echo sprintf("<!ENTITY %-40s SYSTEM '$path'>\n",$name);
+            $entities[] = sprintf("<!ENTITY %-40s SYSTEM '$path'>\n",$name);
           }
         }
       }
