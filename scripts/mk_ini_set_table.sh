8ed2dfa8fec0c50a1a799ad241d8443a14ea7332
Jesus M. Castagnetto <jmcastagnetto@php.net>
2002-11-21 02:22:48 +0000

    Fix for bug #19635
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@105089 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/mk_ini_set_table.sh
+++ scripts/mk_ini_set_table.sh
@@ -2,16 +2,56 @@
 
 # Quick hack to make a table of PHP options
 # and where they can be changed
-# Jesus M. Castagnetto
-# Mon Mar 19 04:57:02 PST 2001
+# Uses GNU grep and GNU awk
+# Author: Jesus M. Castagnetto
+# Created: Mon Mar 19 04:57:02 PST 2001
 # Updated: Thu Apr 25 11:42:26 PDT 2002
 # - look through all PHP_INI_ containing files
 # - save table in the new split dir for the function
+# Updated: Wed Nov 20 18:16:12 PST 2002
+# - rearrange the order of the tables, put the PHP_INI_* definitions first.
 
 cfiles=`grep -rl PHP_INI_ ../php4/*`
 ini_set_table="en/reference/info/functions/ini_set_table";
 
 awk 'BEGIN {
+	print "<note>";
+	print " <para>";
+	print "  The PHP_INI_* constants used in the table below are defined as follows:";
+	print "  <table>";
+	print "   <thead>";
+	print "    <row>";
+	print "     <entry>Constant</entry>";
+	print "     <entry>Value</entry>";
+	print "     <entry>Meaning</entry>";
+	print "    </row>";
+	print "   </thead>";
+	print "   <tbody>";
+	print "    <row>";
+	print "     <entry>PHP_INI_USER</entry>";
+	print "     <entry>1</entry>";
+	print "     <entry>Entry can be set in user scripts</entry>";
+	print "    </row>";
+	print "    <row>";
+	print "     <entry>PHP_INI_PERDIR</entry>";
+	print "     <entry>2</entry>";
+	print "     <entry>Entry can be set in <filename>.htaccess</filename></entry>";
+	print "    </row>";
+	print "    <row>";
+	print "     <entry>PHP_INI_SYSTEM</entry>";
+	print "     <entry>4</entry>";
+	print "     <entry>Entry can be set in <filename>php.ini</filename> or";
+	print "      <filename>httpd.conf</filename></entry>";
+	print "    </row>";
+	print "    <row>";
+	print "     <entry>PHP_INI_ALL</entry>";
+	print "     <entry>7</entry>";
+	print "     <entry>Entry can be set anywhere</entry>";
+	print "    </row>";
+	print "   </tbody>";
+	print "  </table>";
+	print " </para>";
+	print "</note>";
 	print "<table>\n <title>Configuration options</title>"
 	print " <tgroup cols=\"3\">";
 	print "  <thead>";
@@ -47,43 +87,6 @@ END {
 	print "  </tbody>";
 	print " </tgroup>";
 	print "</table>";
-	print "<note>";
-	print " <para>";
-	print "  The PHP_INI_* constants are defined as follows:";
-	print "  <table>";
-	print "   <thead>";
-	print "    <row>";
-	print "     <entry>Constant</entry>";
-	print "     <entry>Value</entry>";
-	print "     <entry>Meaning</entry>";
-	print "    </row>";
-	print "   </thead>";
-	print "   <tbody>";
-	print "    <row>";
-	print "     <entry>PHP_INI_USER</entry>";
-	print "     <entry>1</entry>";
-	print "     <entry>Entry can be set in user scripts</entry>";
-	print "    </row>";
-	print "    <row>";
-	print "     <entry>PHP_INI_PERDIR</entry>";
-	print "     <entry>2</entry>";
-	print "     <entry>Entry can be set in <filename>.htaccess</filename></entry>";
-	print "    </row>";
-	print "    <row>";
-	print "     <entry>PHP_INI_SYSTEM</entry>";
-	print "     <entry>4</entry>";
-	print "     <entry>Entry can be set in <filename>php.ini</filename> or";
-	print "      <filename>httpd.conf</filename></entry>";
-	print "    </row>";
-	print "    <row>";
-	print "     <entry>PHP_INI_ALL</entry>";
-	print "     <entry>7</entry>";
-	print "     <entry>Entry can be set anywhere</entry>";
-	print "    </row>";
-	print "   </tbody>";
-	print "  </table>";
-	print " </para>";
-	print "</note>";
-}' $cfiles > $ini_set_table
+	}' $cfiles > $ini_set_table
 
 ls -l $ini_set_table
