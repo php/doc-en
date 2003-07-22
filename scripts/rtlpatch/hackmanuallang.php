3b5fd10cc5d1445bc4d163bbfc4d249b3ee7a14f
Moshe Doron <momo@php.net>
2003-07-22 05:13:42 +0000

    WS
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@135592 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/rtlpatch/hackmanuallang.php
+++ scripts/rtlpatch/hackmanuallang.php
@@ -1,20 +1,20 @@
 <?
 /*
-  +----------------------------------------------------------------------+
-  | PHP Version 4                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2003 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 2.02 of the PHP licience,     |
-  | that is bundled with this package in the file LICENCE and is         |
-  | avalible through the world wide web at                               |
-  | http://www.php.net/license/2_02.txt.                                 |
-  | If uou did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world wide web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately                |
-  +----------------------------------------------------------------------+
-  | Authors:    Moshe Doron <momo@php.net>                          	 |
-  +----------------------------------------------------------------------+
+  +-------------------------------------------------------------------------------+
+  | PHP Version 4                                                       	    	    |
+  +-------------------------------------------------------------------------------+
+  | Copyright (c) 1997-2003 The PHP Group                                  |
+  +-------------------------------------------------------------------------------+
+  | This source file is subject to version 2.02 of the PHP licience,   |
+  | that is bundled with this package in the file LICENCE and is      |
+  | avalible through the world wide web at                               	    |
+  | http://www.php.net/license/2_02.txt.                                 	    |
+  | If uou did not receive a copy of the PHP license and are unable|
+  |  to obtain it through the world wide web, please send a note to |
+  | license@php.net so we can mail you a copy immediately           |
+  +-------------------------------------------------------------------------------+
+  | Authors:    Moshe Doron <momo@php.net>                          	    |
+  +-------------------------------------------------------------------------------+
   
  $Id$
 */
@@ -24,9 +24,6 @@
 	
 	This file is patch allow per language customize for not pulling the autobuild system all those condition
 	i making use this file to bypass some jade mysteries on the hebrew manual impossible build proccess
-	
-	this script have to be run befor the build proccess.
-	giving the docs path it's edit the files and add dir=rtl,ltr where needed
 	
 	Usage: php scriptname lang-code
 */
