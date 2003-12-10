5592d4e77d218f9cfed534d44b66ca932f3bbc44
Jakub Vrana <vrana@php.net>
2003-12-10 21:30:03 +0000

    Parameter to set output for HTML or PHP
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@146158 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/html_syntax.php
+++ scripts/html_syntax.php
@@ -1,13 +1,12 @@
 #!/usr/bin/php -q
 <?php
-if ($_SERVER["argc"] < 2) {
+if ($_SERVER["argc"] < 3) {
 	exit("Purpose: Syntax highlight PHP examples in DSSSL generated HTML manual.\n"
-		."Usage: html_syntax.php [ filename.ext | dir | wildcard ] ...\n"
+		.'Usage: html_syntax.php [ "html" | "php" ] [ filename.ext | dir | wildcard ] ...' ."\n"
+		.'"html" - highlight_string() is used, "php" - highlight_php() is called' ."\n"
 	);
 }
 set_time_limit(60*60); // can run long, but not more than 1 hour
-
-//~ include dirname(__FILE__) ."/layout.inc"; // we need highlight_php function from /phpweb/include/layout.inc
 
 function callback_html_number_entities_decode($matches) {
 	return chr($matches[1]);
@@ -15,12 +14,16 @@ function callback_html_number_entities_decode($matches) {
 
 function callback_highlight_php($matches) {
 	$with_tags = preg_replace_callback("!&#([0-9]+);!", "callback_html_number_entities_decode", $matches[1]);
-    return highlight_string($with_tags, true);
-	//~ return highlight_php($with_tags, true);
+	if ($GLOBALS["TYPE"] == "php") {
+		return "\n<?php\nhighlight_php('". addcslashes($with_tags, "'\\") ."');\n?>\n";
+	} else { // "html"
+		return highlight_string($with_tags, true);
+	}
 }
 
 $files = $_SERVER["argv"];
 array_shift($files); // $argv[0] - script filename
+$TYPE = array_shift($files); // "html" or "php"
 while (($file = array_shift($files)) !== null) {
 	if (is_file($file)) {
 		$process = array($file);
