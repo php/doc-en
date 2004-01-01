c5feff0c4ba8e724de1f9769b2ca533ed6ac33ca
Gabor Hojtsy <goba@php.net>
2004-01-01 01:08:46 +0000

    even more fixing of licenses and years in the 'happy new year 2004' project
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@147596 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/checkent.php
+++ scripts/checkent.php
@@ -1,5 +1,25 @@
 #!/usr/bin/php -q
 <?php
+/*  
+  +----------------------------------------------------------------------+
+  | PHP Version 4                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2004 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors:    Georg Richter <georg@php.net>                            |
+  |             Gabor Hojtsy <goba@php.net>                              |
+  +----------------------------------------------------------------------+
+ 
+  $Id$
+*/
 
 if ($argc > 1 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
 ?>
@@ -12,9 +32,6 @@ Check entities in entities/global.ent (HTTP and FTP schemes)
   This script checks FTP and HTTP URLs listed
   in entities/global.ent. Grab the output, to put it in
   a text file.
-  
-  Written by Georg Richter <georg@php.net>
-  Adapted to phpdoc by Gabor Hojtsy <goba@php.net>
 
 <?php
   exit;
--- scripts/dbtags.php
+++ scripts/dbtags.php
@@ -1,5 +1,24 @@
 #!/usr/bin/php -q
 <?php
+/*  
+  +----------------------------------------------------------------------+
+  | PHP Version 4                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2004 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors:    Gabor Hojtsy <goba@php.net>                              |
+  +----------------------------------------------------------------------+
+ 
+  $Id$
+*/
 
 if ($argc > 2 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
 ?>
@@ -16,8 +35,6 @@ and give statistics
 
   The script will generate a tag_usage.txt
   file, containing the tags used and the numbers.
-  
-  Written by Gabor Hojtsy <goba@php.net>, 2001-12-28
 
 <?php
   exit;
