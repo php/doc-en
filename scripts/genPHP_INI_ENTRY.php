ed0f208d69ddab195def407f5c73a8e9c9dddde7
Gabor Hojtsy <goba@php.net>
2004-01-01 00:35:42 +0000

    fix years, fix licenses (happy new year 2004 project continued)
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@147594 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/genPHP_INI_ENTRY.php
+++ scripts/genPHP_INI_ENTRY.php
@@ -1,10 +1,28 @@
 <?php
 /*
+  +----------------------------------------------------------------------+
+  | PHP Version 4                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2004 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors:    Jesus M. Castagnetto <jesus@php.net>                     |
+  +----------------------------------------------------------------------+
+
+  $Id$
+*/
+
+/*
  * Translation to PHP of the old script mk_ini_set.sh to
  * generate a list of PHP config options and where they
  * can be set.
- * Author: Jesus M. Castagnetto
- * $Id$
  */
 
 // Global vars for generating the ini.xml
