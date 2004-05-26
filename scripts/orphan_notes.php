df50f4de638ed1bbd4a8f522469d7a9b43a15380
Nuno Lopes <nlopess@php.net>
2004-05-26 19:04:14 +0000

    check for orphan notes script
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@159736 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ scripts/orphan_notes.php
@@ -0,0 +1,112 @@
+<?php
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2004 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors:    Nuno Lopes <nlopess@php.net>                             |
+  +----------------------------------------------------------------------+
+
+ $Id$
+*/
+
+/*
+ * This script searches for orphan notes.
+ * You need a phpweb checkout with, at least,
+ * manual/en and backend/notes folders
+ */
+
+
+/* Configuration Options */
+
+$manual_dir = 'manual/en';
+$notes_dir = 'backend/notes';
+
+/******* END of configurations *****/
+
+
+/* Collect manual IDs */
+function recurse_manual($dir) {
+  global $array;
+
+  if ($dh = opendir($dir)) {
+    while (($file = readdir($dh)) !== false) {
+
+      if($file != '.' && $file != '..') {
+        $path = $dir.'/'.$file;
+
+        if(is_dir($path)) {
+          recurse_manual($path);
+        } else {
+          $array[substr(md5(substr($path, $GLOBALS['len'], -4)), 0, 16)] = 1;
+        }
+
+      }
+    }
+  closedir($dh);
+  }
+}
+
+
+/* Search for bogus notes IDs */
+function recurse_notes($dir) {
+  global $array, $total;
+
+  if ($dh = opendir($dir)) {
+    while (($file = readdir($dh)) !== false) {
+
+      if($file != '.' && $file != '..') {
+        $path = $dir.'/'.$file;
+
+        if(is_dir($path)) {
+          recurse_notes($path);
+        } else {
+          if(!isset($array[$file]) && $file != 'last-updated' && $file != 'sections') {
+            echo "file: $path\n";
+
+            $fp = fopen($path, "r");
+            while (!feof($fp)) {
+              $line = chop(fgets($fp, 12288));
+              if ($line == "") { continue; }
+
+              list($id, $sect, , , , ) = explode("|", $line);
+
+              if (!isset($done)) {
+                $done = 1;
+                echo "old ID: $sect\nNotes IDs: $id";
+
+              } else {
+                echo ", $id";
+              }
+            }
+            echo "\n\n";
+            $total++;
+          }
+
+        unset($done);
+        }
+      }
+    }
+  closedir($dh);
+  }
+}
+
+$array = array();
+$len = strlen("$manual_dir/");
+$total = 0;
+
+recurse_manual($manual_dir);
+recurse_notes($notes_dir);
+
+
+echo "\nTotal: $total";
+?>
