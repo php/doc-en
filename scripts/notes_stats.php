3f9c1224b07e9705948057c2cdd1ed20934e5478
Vincent Gevers <vincent@php.net>
2004-06-28 08:22:40 +0000

    Adding notes_stats.php script. Current output can be found at http://notes.allowee.no-ip.com/output.php
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@162213 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ scripts/notes_stats.php
@@ -0,0 +1,393 @@
+<?php
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2004 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors:    Vincent Gevers <vincent@php.net>                         |
+  +----------------------------------------------------------------------+
+
+ $Id$
+*/
+
+/*
+ * Usage:
+ * $ php notes_stats.php > notes.php
+ */
+
+// minimum amount actions
+$minact = 100;
+// after how many secs should the list be chopped
+$after = 182.5*24*60*60; // half year
+
+$s = nntp_connect("news.php.net")
+  or die("failed to connect to news server");
+
+$res = nntp_cmd($s,"GROUP php.notes",211)
+  or die("failed to get infos on news group");
+
+$new = explode(" ", $res);
+$first = 1;
+$last =  $new[0];
+
+$time_start = getmicrotime();
+
+$res = nntp_cmd($s,"XOVER $first-$last", 224)
+    or die("failed to XOVER the new items");
+
+$files = $team = $tmp = array();
+
+$in_old = false;
+
+for ($i = $first; $i < $last; $i++) {
+$line = fgets($s, 4096);
+ list($n,$subj,$author,$odate,$messageid,$references,$bytes,$lines,$extra)= explode("\t", $line, 9);
+
+
+//$reg = '/^note (\d*) (.*) from ([^ ]*) by ([a-z]*)/';
+$reg = '/^note (\d*) (.*) (?:from|in) ([^ ]*) by ([a-z]*)/';
+
+if (preg_match($reg, $subj, $d)) {
+    if ($d[2] == 'rejected and deleted')
+        $d[2] = 'rejected';
+    if ($d[2] == 'approved')
+        continue;
+        
+    if(calc_time($odate)) {
+        // 'new' before $after
+        @$team['n'][$d[4]]['total']++;
+        @$team['n'][$d[4]][$d[2]]++; 
+        @$tmp['n'][$d[4]]++;
+        @$files['n'][$d[3]]++;
+    } else {
+        // 'old' after $after
+        @$team['o'][$d[4]][$d[2]]++; 
+        @$tmp['o'][$d[4]]++;
+        @$team['o'][$d[4]]['total']++; 
+        @$files['o'][$d[3]]++;
+    }
+       
+        // the normal arrays
+        @$team[$d[4]]['total']++;
+        @$team[$d[4]][$d[2]]++; 
+        @$tmp[$d[4]]++;
+        @$files[$d[3]]++;
+    
+    } // end if(preg_match
+    if ($i == $last) {
+        break;
+   }
+}
+
+ksort($team);
+arsort($team['n']);
+arsort($team['o']);
+arsort($files);
+arsort($tmp);
+
+echo '<html>
+<head>
+<title>Note Statistics for '.date('j F Y').'</title>
+</head>
+<body>
+';
+
+echo '
+<b>' . array_sum($files) . '</b> subjects parsed<br /><br />
+';
+
+?>
+<table border='0' cellspacing="10"><tr valign="top"><td valign="top">
+<table border='1'>
+    <tr>
+        <td colspan="5" align="center">Editors Stats with more then <?php echo $minact; ?> actions</td>
+    </tr>
+    <tr>
+        <td>user</td>
+        <td>deleted</td>
+        <td>rejected</td>
+        <td>modified</td>
+        <td>total</td>
+    </tr>
+
+<?php
+
+$bg = '#EBEBEB';
+
+foreach ($team as $user => $actions) {
+    if($user == 'o' or $user =='n')
+       continue;
+    
+    if($actions['total'] >= $minact) { 
+        echo "<tr bgcolor=\"";
+        $bg = ($bg == '#EBEBEB') ? '#BEBEBE' : '#EBEBEB';
+        echo "$bg\">\n\t<td>$user</td>\n\t<td>";
+        echo isset($actions['deleted']) ? $actions['deleted'] : '0';
+        echo "</td>\n\t<td>";
+        echo isset($actions['rejected']) ? $actions['rejected'] : '0';
+        echo "</td>\n\t<td>";
+        echo isset($actions['modified']) ? $actions['modified'] : '0';
+        echo "</td>\n\t<td>";
+        echo $actions['total'];
+        echo "</td>\n</tr>\n";
+    }
+    
+}
+
+
+?>
+</table>
+
+</td><td valign="top">
+Last half year (with more then <?php echo $minact; ?> actions counted)
+<table border='1'>
+    <tr>
+        <td colspan="5" align="center">Recent Editors stats</td>
+    </tr>
+    <tr>
+        <td>user</td>
+        <td>deleted</td>
+        <td>rejected</td>
+        <td>modified</td>
+        <td>total</td>
+    </tr>
+
+<?php
+
+$bg = '#EBEBEB';
+foreach ($team['n'] as $user => $actions) {
+
+    if($actions['total'] >= $minact) {    
+        echo "<tr bgcolor=\"";
+        $bg = ($bg == '#EBEBEB') ? '#BEBEBE' : '#EBEBEB';
+        echo "$bg\">\n\t<td>$user</td>\n\t<td>";
+        echo isset($actions['deleted']) ? $actions['deleted'] : '0';
+        echo "</td>\n\t<td>";
+        echo isset($actions['rejected']) ? $actions['rejected'] : '0';
+        echo "</td>\n\t<td>";
+        echo isset($actions['modified']) ? $actions['modified'] : '0';
+        echo "</td>\n\t<td>";
+        echo $actions['total'];
+        echo "</td>\n</tr>\n";
+    }
+
+}
+
+
+?>
+</table>
+
+<br />
+Before the last half year (with more then <?php echo $minact; ?> actions counted)
+<table border='1'>
+    <tr>
+        <td colspan="5" align="center">Older Editors Stats</td>
+    </tr>
+    <tr>
+        <td>user</td>
+        <td>deleted</td>
+        <td>rejected</td>
+        <td>modified</td>
+        <td>total</td>
+    </tr>
+
+<?php
+
+$bg = '#EBEBEB';
+foreach ($team['o'] as $user => $actions) {
+
+    if($actions['total'] >= $minact) {
+        echo "<tr bgcolor=\"";
+        $bg = ($bg == '#EBEBEB') ? '#BEBEBE' : '#EBEBEB';
+        echo "$bg\">\n\t<td>$user</td>\n\t<td>";
+        echo isset($actions['deleted']) ? $actions['deleted'] : '0';
+        echo "</td>\n\t<td>";
+        echo isset($actions['rejected']) ? $actions['rejected'] : '0';
+        echo "</td>\n\t<td>";
+        echo isset($actions['modified']) ? $actions['modified'] : '0';
+        echo "</td>\n\t<td>";
+        echo $actions['total'];
+        echo "</td>\n</tr>\n";
+    }
+
+}
+
+
+?>
+</table>
+</td></tr></table>
+
+
+<br />
+
+<table border="0" cellspacing="10"><tr valign="top"><td valign="top">
+<table border='1'>
+    <tr>
+        <td colspan="5" align="center">Total Editors Stats</td>
+    </tr>
+    <tr>
+        <td>user</td>
+        <td>deleted</td>
+        <td>rejected</td>
+        <td>modified</td>
+        <td>total</td>
+    </tr>
+
+<?php
+
+$bg = '#EBEBEB';
+
+foreach ($team as $user => $actions) {
+    if($user == 'o' or $user =='n')
+       continue;
+       
+    echo "<tr bgcolor=\"";
+    $bg = ($bg == '#EBEBEB') ? '#BEBEBE' : '#EBEBEB';
+    echo "$bg\">\n\t<td>$user</td>\n\t<td>";
+    echo isset($actions['deleted']) ? $actions['deleted'] : '0';
+    echo "</td>\n\t<td>";
+    echo isset($actions['rejected']) ? $actions['rejected'] : '0';
+    echo "</td>\n\t<td>";
+    echo isset($actions['modified']) ? $actions['modified'] : '0';
+    echo "</td>\n\t<td>";
+    echo $actions['total'];
+    echo "</td>\n</tr>\n";
+
+}
+
+
+?>
+</table>
+
+</td><td valign="top">
+<table border='1'>
+    <tr>
+        <td colspan="3" align="center">Editors top 15</td>
+    </tr>
+    <tr>
+        <td>rank</td>
+        <td>user</td>
+        <td>total</td>
+    </tr>
+<?php
+$i = 0;
+    foreach($tmp as $k => $v) {
+    if($k == 'o' or $k =='n')
+       continue;
+       
+        $i++;
+        echo "<tr bgcolor=\"";
+        $bg = ($bg == '#EBEBEB') ? '#BEBEBE' : '#EBEBEB'; 
+        echo "$bg\">\n\t<td>$i</td>\n\t<td>$k</td>\n\t<td>$v</td>\n</tr>\n";
+    
+    if ($i == 15)
+        break;
+}       
+
+?>
+</table>
+
+<br />
+<table border='1'>
+    <tr>
+        <td colspan="3" align="center">Manual pages most active top 20</td>
+    </tr>
+    <tr>
+        <td>rank</td>
+        <td>page</td>
+        <td>total</td>
+    </tr>
+<?php
+$i = 0;
+    foreach($files as $k => $v) {
+    if($k == 'o' or $k =='n')
+       continue;
+       
+        $i++;
+        echo "<tr bgcolor=\"";
+       $bg = ($bg == '#EBEBEB') ? '#BEBEBE' : '#EBEBEB'; 
+        echo "$bg\">\n\t<td>$i</td>\n\t<td>$k</td>\n\t<td>$v</td>\n</tr>\n";
+    
+    if ($i == 20)
+        break;
+}       
+
+?>
+</table>
+
+</td></tr></table>
+
+<?php
+ $time_end = getmicrotime();
+ $time = $time_end - $time_start;
+ 
+ echo "All done in $time seconds\n";
+ ?>
+</body>
+</html>
+
+<?php
+
+function nntp_connect($server,$port=119) {
+  $s = fsockopen($server,$port,$errno,$errstr,30);
+
+  if (!$s) {
+    echo "<!-- error connecting to nntp server: $errstr -->\n";
+    return false;
+  }
+  $hello = fgets($s, 1024);
+  if (substr($hello,0,4) != "200 ") {
+    echo "<!-- unexpected greeting: $hello -->\n";
+    return false;
+  }
+  #echo "<!-- $hello -->\n";
+  return $s;
+}
+
+function nntp_cmd($conn,$command,$expected) {
+  if (strlen($command) > 510) die("command too long: $command");
+  fputs($conn, "$command\r\n");
+  $res = fgets($conn, 1024);
+  list($code,$extra) = explode(" ", $res, 2);
+  return $code == $expected ? $extra : false;
+}
+
+function getmicrotime() { 
+    list($usec, $sec) = explode(" ", microtime()); 
+    return ((float)$usec + (float)$sec); 
+} 
+
+function calc_time ($before) {
+global $after;
+
+// simple caching
+if(@$in_old == true) 
+   return false;
+
+$before = strtotime($before);
+
+$afterall =  $before - (time() - $after);
+
+if($afterall > 0) {
+    // more then $after
+    $in_old = false;    
+    return true;
+} else {
+    // older then $after
+    $in_old = true;
+    return false;
+}
+
+}
+
+
+?>
