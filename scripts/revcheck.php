b6214f12b6e71e56796cd3304a5d7574edd5020b
Mehdi Achour <didou@php.net>
2004-10-29 14:20:44 +0000

    Index: scripts/revcheck.php
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@171558 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/revcheck.php
+++ scripts/revcheck.php
@@ -727,6 +727,8 @@ print ($navbar."<p>&nbsp;</p>\n");
 // Files table goes here
 // =========================================================================
 
+if (count($files_status) != 0) {
+
 print <<<END_OF_MULTILINE
 <a name="files"></a>
 <table width="820" border="0" cellpadding="4" cellspacing="1" align="center">
@@ -751,69 +753,71 @@ print <<<END_OF_MULTILINE
 </tr>
 END_OF_MULTILINE;
 
-// This was the previous directory [first]
-$prev_dir = $new_dir = $DOCDIR."en";
-
-// Go through all files collected
-foreach ($files_status as $num => $file) {
-
-    // Make the maintainer a link, if we have that maintainer in the list
-    if (isset($maint_by_nick[$file["maintainer"]])) {
-      $file["maintainer"] = '<a href="#maint' . $maint_by_nick[$file["maintainer"]] .
-                            '">' . $file["maintainer"] . '</a>';
-    }
-
-    // If we have a 'numeric' revision diff and it is not zero,
-    // make a link to the CVS repository's diff script
-    if ($file["revision"][2] != "n/a" && $file["revision"][2] !== 0) {
-        $file["short_name"] = "<a href=\"http://cvs.php.net/diff.php/" .
-                              preg_replace( "'^".$DOCDIR."'", "phpdoc/", $file["full_name"]) .
-                              "?r1=" . $file["revision"][1] . 
-                              "&amp;r2=" . $file["revision"][0] .
-                              CVS_OPT . "\">" . $file["short_name"] . "</a>";
-    }
-
-    // Guess the new directory from the full name of the file
-    $new_dir = dirname($file["full_name"]);
+    // This was the previous directory [first]
+    $prev_dir = $new_dir = $DOCDIR."en";
     
-    // If this is a new directory, put out old dir lines
-    if ($new_dir != $prev_dir && isset($lines)) {
-        echo $prev_diplay_dir;
-        echo " ($line_number)</th></tr>";
-	echo $lines;
-	
-	$lines = '';
-	$line_number = 0;
+    // Go through all files collected
+    foreach ($files_status as $num => $file) {
+    
+        // Make the maintainer a link, if we have that maintainer in the list
+        if (isset($maint_by_nick[$file["maintainer"]])) {
+          $file["maintainer"] = '<a href="#maint' . $maint_by_nick[$file["maintainer"]] .
+                                '">' . $file["maintainer"] . '</a>';
+        }
+    
+        // If we have a 'numeric' revision diff and it is not zero,
+        // make a link to the CVS repository's diff script
+        if ($file["revision"][2] != "n/a" && $file["revision"][2] !== 0) {
+            $file["short_name"] = "<a href=\"http://cvs.php.net/diff.php/" .
+                                  preg_replace( "'^".$DOCDIR."'", "phpdoc/", $file["full_name"]) .
+                                  "?r1=" . $file["revision"][1] . 
+                                  "&amp;r2=" . $file["revision"][0] .
+                                  CVS_OPT . "\">" . $file["short_name"] . "</a>";
+        }
+    
+        // Guess the new directory from the full name of the file
+        $new_dir = dirname($file["full_name"]);
         
-        // Store the new actual directory
-        $prev_dir = $new_dir;
-    }
-    // Drop out the unneeded parts from the dirname...
-    $display_dir = str_replace($DOCDIR."en/", "", dirname($file["full_name"]));
-    $prev_diplay_dir = "<tr class=blue><th colspan=12>$display_dir";
+        // If this is a new directory, put out old dir lines
+        if ($new_dir != $prev_dir && isset($lines)) {
+            echo $prev_diplay_dir;
+            echo " ($line_number)</th></tr>";
+    	echo $lines;
+    	
+    	$lines = '';
+    	$line_number = 0;
+            
+            // Store the new actual directory
+            $prev_dir = $new_dir;
+        }
+        // Drop out the unneeded parts from the dirname...
+        $display_dir = str_replace($DOCDIR."en/", "", dirname($file["full_name"]));
+        $prev_diplay_dir = "<tr class=blue><th colspan=12>$display_dir";
+        
+        // Save the line for the current file (get file name shorter)
+        $lines .= "<tr class={$CSS[$file['mark']]}><td>{$file['short_name']}</td>".
+              "<td> {$file['revision'][0]}</td>" .
+              "<td> {$file['revision'][1]}</td>".
+              "<td class=rb>{$file['revision'][2]} </td>".
+              "<td class=r>{$file['size'][0]} </td>".
+              "<td class=r>{$file['size'][1]} </td>".
+              "<td class=rb>{$file['size'][2]} </td>".
+              "<td class=r>{$file['date'][0]} </td>".
+              "<td class=r>{$file['date'][1]} </td>".
+              "<td class=rb>{$file['date'][2]} </td>".
+              "<td class=c>{$file['maintainer']}</td>".
+              "<td class=c>".trim($file['status'])."</td></tr>\n";
+         $line_number++;
     
-    // Save the line for the current file (get file name shorter)
-    $lines .= "<tr class={$CSS[$file['mark']]}><td>{$file['short_name']}</td>".
-          "<td> {$file['revision'][0]}</td>" .
-          "<td> {$file['revision'][1]}</td>".
-          "<td class=rb>{$file['revision'][2]} </td>".
-          "<td class=r>{$file['size'][0]} </td>".
-          "<td class=r>{$file['size'][1]} </td>".
-          "<td class=rb>{$file['size'][2]} </td>".
-          "<td class=r>{$file['date'][0]} </td>".
-          "<td class=r>{$file['date'][1]} </td>".
-          "<td class=rb>{$file['date'][2]} </td>".
-          "<td class=c>{$file['maintainer']}</td>".
-          "<td class=c>".trim($file['status'])."</td></tr>\n";
-     $line_number++;
-
+    }
+    
+    // echo the last dir and $lines
+    echo "$prev_diplay_dir ($line_number)</th></tr>";
+    echo $lines;
+    
+    print("</table>\n<p>&nbsp;</p>\n$navbar<p>&nbsp;</p>\n");
+    
 }
-
-// echo the last dir and $lines
-echo "$prev_diplay_dir ($line_number)</th></tr>";
-echo $lines;
-
-print("</table>\n<p>&nbsp;</p>\n$navbar<p>&nbsp;</p>\n");
 
 
 // =========================================================================
