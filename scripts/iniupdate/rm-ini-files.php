da668ddfc6bbfbc6eedc8ac8b306a28934d32a1c
Nuno Lopes <nlopess@php.net>
2005-03-26 13:40:41 +0000

    add new script to help in the debugging
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@182912 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ scripts/iniupdate/rm-ini-files.php
@@ -0,0 +1,17 @@
+<?php
+
+/* this script will delete all files that can be modified by
+   the ini updater. It's very usefull when debugging! */
+
+$phpdoc_dir  = '../..';
+
+$ini_files   = glob("$phpdoc_dir/en/reference/*/ini.xml");
+$ini_files[] = "$phpdoc_dir/en/features/safe-mode.xml";
+$ini_files[] = "$phpdoc_dir/en/appendices/ini.xml";
+
+foreach ($ini_files as $file) {
+    unlink($file);
+    echo "Deleted $file\n";
+}
+
+?>
