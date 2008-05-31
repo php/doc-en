f1876c15d6dc9826a799d012555b06dd82e86996
Salah Faya <visualmind@php.net>
2008-05-31 09:23:35 +0000

    PHP License header
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@260494 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/online_editor/base.php
+++ scripts/online_editor/base.php
@@ -1,10 +1,24 @@
 <?php
-//*-- The PHPDOC Online XML Editing Tool 
-//*-- Developed by Salah Faya visualmind(@)php.net 
-//*-- Version 1.0 - essentially developed for Arabic Translation of PHP Manual
-//*-- Now updated to work with all phpdoc translations
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 4                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2008 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors : Salah Faya <visualmind@php.net>                            |
+  +----------------------------------------------------------------------+
 
-//--- base file (all other files require this to start)
+*/
+//-- The PHPDOC Online XML Editing Tool 
+//--- Purpose: base file (all other files require this to start)
 
 //------- Configuration 
 // Root path to the phpdoc-all directory
--- scripts/online_editor/login.php
+++ scripts/online_editor/login.php
@@ -1,10 +1,24 @@
 <?php
-//*-- The PHPDOC Online XML Editing Tool 
-//*-- Developed by Salah Faya visualmind(@)php.net 
-//*-- Version 1.0 - essentially developed for Arabic Translation of PHP Manual
-//*-- Now updated to work with all phpdoc translations
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 4                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2008 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.0 of the PHP license,       |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_0.txt.                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors : Salah Faya <visualmind@php.net>                            |
+  +----------------------------------------------------------------------+
 
-//--- This file allows users to login using an email address (TODO we may use CVS or other way in future)
+*/
+//-- The PHPDOC Online XML Editing Tool 
+//--- Purpose: allows users to login using an email address (TODO we may use CVS or other way in future)
 
 //------- Initialization
 require 'base.php';
