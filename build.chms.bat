d4aa9fe01de6fe2d522bbeb0ef3ecd2051c0c05b
Hannes Magnusson <bjori@php.net>
2008-12-03 20:11:59 +0000

    - Add support for multiple languages - Make sure the configure.php finished successfully
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@270486 c90b9560-bf6c-de11-be94-00142212c4b1

--- build.chms.bat
+++ build.chms.bat
@@ -5,24 +5,31 @@ SET HH=C:\Program Files (x86)\HTML Help Workshop\hhc.exe
 
 cd c:\phpdoc\
 
-ECHO CVS Checkout...
-"C:\Program Files (x86)\CVSNT\cvs" up > cvs.log 2<&1
+ECHO CVS Updating...
+REM "C:\Program Files (x86)\CVSNT\cvs" up > logs\cvs.log 2<&1
 
-ECHO Configuring...
-"%PHP%" configure.php --with-php=%PHP% > configure.log 2<&1
+FOR %%A IN (en ja fr) DO (
+	ECHO Configuring %%A...
+	"%PHP%" configure.php --with-php=%PHP% --with-lang=%%A > logs\configure.%%A.log 2<&1
 
-ECHO Running PHD...
-CALL "%PHD%" -d .manual.xml -f xhtml -t chmsource -o tmp > phd.log 2<&1
+	IF EXIST .manual.xml (
+		ECHO Running PhD...
+		CALL "%PHD%" -d .manual.xml -f xhtml -t chmsource -o tmp --lang %%A > logs\phd.%%A.log 2<&1
 
-ECHO Generating the chm...
-"%HH%" tmp/chm/php_manual_en.hhp   > hhc.log 2<&1
-move tmp\chm\php_manual_en.chm chmfiles/
+		ECHO Generating the chm...
+		"%HH%" tmp/chm/php_manual_%%A.hhp > logs\hhc.%%A.log 2<&1
+		move tmp\chm\php_manual_%%A.chm chmfiles/
+	) ELSE (
+		echo Build error!
+	)
 
-ECHO Cleaning temp files...
-del /F /Q C:\phpdoc\tmp
-rmdir /s /q C:\phpdoc\tmp\chm\res
-rmdir /s /q C:\phpdoc\tmp\chm\
-rmdir /s /q C:\phpdoc\tmp\html
-rmdir /s /q C:\phpdoc\tmp
+	ECHO Cleaning temp files...
+	del /F /Q C:\phpdoc\tmp
+	del /Q C:\phpdoc\.manual.xml
+	rmdir /s /q C:\phpdoc\tmp\chm\res
+	rmdir /s /q C:\phpdoc\tmp\chm\
+	rmdir /s /q C:\phpdoc\tmp\html
+	rmdir /s /q C:\phpdoc\tmp
+)
 
 echo Done!
