c7933f06fcdb96c444ebf2ebe58cd9c0d0ba4668
Hannes Magnusson <bjori@php.net>
2008-12-04 19:48:01 +0000

    Automagically upload the chm to the rsync box
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@270543 c90b9560-bf6c-de11-be94-00142212c4b1

--- build.chms.bat
+++ build.chms.bat
@@ -2,11 +2,12 @@
 SET PHP=c:\php53\php.exe
 SET PHD=C:\pear\phd.bat
 SET HH=C:\Program Files (x86)\HTML Help Workshop\hhc.exe
-
+SET CP=C:\Program Files (x86)\PuTTY\pscp.exe
+SET PRIVATES=C:\Documents and Settings\bjori\My Documents\my.privates.ppk
 cd c:\phpdoc\
 
 ECHO CVS Updating...
-REM "C:\Program Files (x86)\CVSNT\cvs" up > logs\cvs.log 2<&1
+"C:\Program Files (x86)\CVSNT\cvs" up > logs\cvs.log 2<&1
 
 FOR %%A IN (en ja fr) DO (
 	ECHO Configuring %%A...
@@ -18,6 +19,9 @@ FOR %%A IN (en ja fr) DO (
 
 		ECHO Generating the chm...
 		"%HH%" tmp/chm/php_manual_%%A.hhp > logs\hhc.%%A.log 2<&1
+
+		"%CP%" -batch -q -i "%PRIVATES%"  -l bjori tmp\chm\php_manual_%%A.chm rsync.php.net:/home/bjori/
+
 		move tmp\chm\php_manual_%%A.chm chmfiles/
 	) ELSE (
 		echo Build error!
