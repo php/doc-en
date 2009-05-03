a2a29e8bccac684777b78f07246bbbc8684793b8
Kalle Sommer Nielsen <kalle@php.net>
2009-05-03 00:53:23 +0000

    Fix typo
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@279750 c90b9560-bf6c-de11-be94-00142212c4b1

--- scripts/quickref/cat.php
+++ scripts/quickref/cat.php
@@ -13,7 +13,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author:     Kalle Sommer Nielsen <kalle@php.net                      |
+  | Author:     Kalle Sommer Nielsen <kalle@php.net>                     |
   +----------------------------------------------------------------------+
   
   $Id$
