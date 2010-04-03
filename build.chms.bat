6813848a729eaf3ccc1fdf74c64d0bea022ceeb6
Hannes Magnusson <bjori@php.net>
2010-04-03 16:20:24 +0000

    Use PhD1.0 ways..
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@297425 c90b9560-bf6c-de11-be94-00142212c4b1

--- build.chms.bat
+++ build.chms.bat
@@ -14,7 +14,7 @@ cd c:\doc-all\
 ECHO SVN Updating...
 "%SVN%" up > logs\cvs.log 2<&1
 ECHO Upgrading PhD
-CALL "%PEAR%" upgrade doc.php.net/phd-beta > logs\pear.log 2<&1
+CALL "%PEAR%" upgrade doc.php.net/phd > logs\pear.log 2<&1
 
 FOR %%A IN (en bg de es fa fr ja kr pl pt_BR ro ru tr) DO (
 	ECHO Configuring %%A...
@@ -22,14 +22,14 @@ FOR %%A IN (en bg de es fa fr ja kr pl pt_BR ro ru tr) DO (
 
 	IF EXIST doc-base/.manual.xml (
 		ECHO Running PhD...
-		CALL "%PHD%" -d doc-base/.manual.xml -f xhtml -t chmsource -o tmp --lang %%A > logs\phd.%%A.log 2<&1
+		CALL "%PHD%" -d doc-base/.manual.xml -P PHP -f chm -o tmp --lang %%A > logs\phd.%%A.log 2<&1
 
 		ECHO Generating the chm...
-		"%HH%" tmp/chm/php_manual_%%A.hhp > logs\hhc.%%A.log 2<&1
+		"%HH%" tmp/php-chm/php_manual_%%A.hhp > logs\hhc.%%A.log 2<&1
 
-		"%CP%" -batch -q -i "%PRIVATES%"  -l bjori c:\doc-all\tmp\chm\php_manual_%%A.chm rsync.php.net:/home/bjori/manual-chms-new/
+		"%CP%" -batch -q -i "%PRIVATES%"  -l bjori c:\doc-all\tmp\php-chm\php_manual_%%A.chm rsync.php.net:/home/bjori/manual-chms-new/
 
-		move tmp\chm\php_manual_%%A.chm chmfiles/
+		move tmp\php-chm\php_manual_%%A.chm chmfiles/
 	) ELSE (
 		echo Build error!
 	)
@@ -37,8 +37,8 @@ FOR %%A IN (en bg de es fa fr ja kr pl pt_BR ro ru tr) DO (
 	ECHO Cleaning temp files...
 	del /F /Q C:\doc-all\tmp
 	del /Q C:\doc-all\doc-base\.manual.xml
-	rmdir /s /q C:\doc-all\tmp\chm\res
-	rmdir /s /q C:\doc-all\tmp\chm\
+	rmdir /s /q C:\doc-all\tmp\php-chm\res
+	rmdir /s /q C:\doc-all\tmp\php-chm\
 	rmdir /s /q C:\doc-all\tmp\html
 	rmdir /s /q C:\doc-all\tmp
 )
