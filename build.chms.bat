6a8b66b241e881db7423faeb47a1188f85d8fde0
Pierre Joye <pajoye@php.net>
2010-06-02 22:22:17 +0000

    - disable memory limit
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@300114 c90b9560-bf6c-de11-be94-00142212c4b1

--- build.chms.bat
+++ build.chms.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 SET PHP_DOC_BASE_DIR=%~dp0
-CALL "C:\php\binaries\PHP_5_3\php.exe" "%PHP_DOC_BASE_DIR%\scripts\build-chms.php" %*
+CALL "C:\php\binaries\PHP_5_3\php.exe" -d memory_limit=-1 "%PHP_DOC_BASE_DIR%\scripts\build-chms.php" %*
