837e783084c0d717204fe5c09582ba2353724e3d
Richard Quadling <rquadling@php.net>
2011-05-04 15:48:38 +0000

    Make sure logs directory always exists prior to use.
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@310762 c90b9560-bf6c-de11-be94-00142212c4b1

--- build.chms.bat
+++ build.chms.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 SET PHP_DOC_BASE_DIR=%~dp0
 svn update scripts/build-chms.php > nul
+IF NOT EXIST "%PHP_DOC_BASE_DIR%\..\logs" MD "%PHP_DOC_BASE_DIR%\..\logs"
 CALL "C:\php\binaries\PHP_5_3\php.exe" -d memory_limit=-1 "%PHP_DOC_BASE_DIR%\scripts\build-chms.php" %* > "%PHP_DOC_BASE_DIR%\..\logs\%~n0.log"
