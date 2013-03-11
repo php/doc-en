f0de981556ec8e8bdadce1bc251a12485829d2fd
Kalle Sommer Nielsen <kalle@php.net>
2013-03-11 08:30:07 +0000

    We need to sync build-chms-config.php here too
    git-svn-id: https://svn.php.net/repository/phpdoc/doc-base/trunk@329734 c90b9560-bf6c-de11-be94-00142212c4b1

--- build.chms.bat
+++ build.chms.bat
@@ -2,5 +2,6 @@
 
 SET PHP_DOC_BASE_DIR=%~dp0
 
+CALL svn update scripts/build-chms-config.php > nul
 CALL svn update scripts/build-chms.php > nul
 CALL "E:\software\php\php.exe" -d memory_limit=-1 "%PHP_DOC_BASE_DIR%\scripts\build-chms.php" %*
