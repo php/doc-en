84830b2289a03385f695a4b274a2fc78ae34e257
haszi <haszika80@gmail.com>
2024-02-29 09:00:17 +0100

    Revert "Add bash script for parle constant ID update"
    This reverts commit 1f7c5d41dc58872cadb966fc07a5a5d594cd132b.

--- parle_constant_ids.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-\grep -lrE --include='*.xml' '"parle\.constant\.utf32"' | xargs -d '\n' sed -i 's/"parle\.constant\.utf32"/"constant\.parle-internal-utf32"/g'
-\grep -lrE --include='*.xml' '"parle(.)+\.constants\.flag-regex-' | xargs -d '\n' sed -i 's/\.constants\.flag-regex-/\.constants\./g'
