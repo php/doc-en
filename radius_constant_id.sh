344c9675dfa2e1b522ac23860cddf80ef66fbd09
haszi <haszika80@gmail.com>
2024-02-29 08:56:49 +0100

    Revert "Add bash script for Radius constant ID update"
    This reverts commit d67a121a4992ffb5afaa4b60bf31c4f5c2b5904d.

--- radius_constant_id.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-\grep -lrE --include='*.xml' '"radius\.constants\.radius-mppe-key-len"' | xargs -d '\n' sed -i 's/"radius\.constants\.radius-mppe-key-len"/"constant.radius-mppe-key-len"/g'
