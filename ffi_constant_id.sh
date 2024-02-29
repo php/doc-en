96e58b3734d7ea5bc8150962ffa555ac421912bc
haszi <haszika80@gmail.com>
2024-02-29 09:09:24 +0100

    Revert "Add bash script for FFI constant ID update"
    This reverts commit 107ea22ba6d9fa6c330fa3c808b20f5c1c4380d7.

--- ffi_constant_id.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-\grep -lrE --include='*.xml' '"ffi-ffi\.constants\.biggest-alignment"' | xargs -d '\n' sed -i 's/"ffi-ffi\.constants\.biggest-alignment"/"ffi\.constants\.--biggest-alignment--"/g'
